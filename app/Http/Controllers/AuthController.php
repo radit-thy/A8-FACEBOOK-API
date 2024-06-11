dc73adfbbb8cd03ae0ce1906fe9100323331f977
Radit They <rdit.thy@student.passerellesnumeriques.org>
2024-06-11 20:29:28 +0700

    register create an account #4

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,10 +1,40 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Auth;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
     //
+    // ======================================================
+    //                 Registeration
+    // ======================================================
+    public function register(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name'      =>'required|string|max:255',
+            'email'     => 'required|string|max:255|unique:users',
+            'password'  => 'required|string'
+          ]);
+
+        if ($validator->fails()) {
+            return response()->json(["message"=>$validator->errors()],500);
+        }
+        $name=$request->get("name");
+        $email=$request->get("email");
+        $password=$request->get("password");
+        $isRegister=User::create(["name"=>$name,"email"=>$email,"password"=>$password]);
+        if($isRegister){
+            return response(["data"=>$isRegister,"message"=>"Register has been successfuly"],200);
+        }else{
+            return response(["data"=>$isRegister,"message"=>"Register is not successfuly"],500);
+        }
+
+    }
+
+    
 }
