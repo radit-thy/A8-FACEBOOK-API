951d6be9d16917eaea0e6484d7b6688b860e9d6b
CHANDY MOEUN <chandy.moeun@student.passerellesnumeriques.org>
2024-06-13 08:22:13 +0700

    profile

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -5,7 +5,7 @@ namespace App\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class ProfileRescource extends JsonResource
+class UserResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -15,8 +15,10 @@ class ProfileRescource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            "image" => $this->image,
-            "user"=>$this->user
+            'user_id' => $this->id,
+            'username' => $this->name,
+            'email' => $this->email,
+            'profile'=>$this->profile
         ];
     }
 }
